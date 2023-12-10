0adca89499d1fb2fa2443d50a6050f2f37ebf82e
Diego Gonzalez <diegonzalezdevgmail.com>
2023-12-10 19:18:23 -0300

    Using Alternative Variable Declaration Styles

--- GettingStarted/investment_calculator.go
+++ GettingStarted/investment_calculator.go
@@ -5,11 +5,11 @@ import (
 	"math"
 )
 func main() {
-	var investmentAmount float64 =1000
-	var expectedReturnRate=5.5
-	var years float64=10
+	var investmentAmount float64=1000 
+	years:= 10.0
+	expectedReturnRate := 5.5
 
-	var futureValue= investmentAmount*math.Pow(1+expectedReturnRate/100,years)
+	futureValue := investmentAmount*math.Pow(1+expectedReturnRate/100,years)
 	
 	fmt.Println(futureValue)
 }
