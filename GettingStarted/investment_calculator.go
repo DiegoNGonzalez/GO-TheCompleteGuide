db9341e0e4e3ddee8d19dbea98ece3f4018d192e
Diego Gonzalez <diegonzalezdevgmail.com>
2023-12-10 19:52:43 -0300

    fmt Scan

--- GettingStarted/investment_calculator.go
+++ GettingStarted/investment_calculator.go
@@ -5,10 +5,12 @@ import (
 	"math"
 )
 func main() {
-	const inflationRate =2.5
-	var investmentAmount float64=1000 
+	const inflationRate =6.5
+	var investmentAmount float64
 	years:= 10.0
 	expectedReturnRate := 5.5
+
+	fmt.Scan(&investmentAmount)
 
 	futureValue := investmentAmount*math.Pow(1+expectedReturnRate/100,years)
 	
